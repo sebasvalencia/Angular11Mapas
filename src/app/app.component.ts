5ca4a3c3923a7b9f3f38ba548f5b42f8e1259686
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-24 19:36:07 -0500

    Onceava app en Angular - Mapas App - Creamos una interfaz de tipo marcador, eventos en el mapa, crear marcadores, arrastrarlos y obtener coordenadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { MapasService } from './services/mapas.service';
+
+import { Marcador } from './interfaces/marcador.interface';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +13,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // datos por defecto - frisby de la 70
-  lat: number= 6.246062;
-  lng: number = -75.589227;
-  //zoom: 17;
+  lat = 6.246062;
+  lng = -75.589227;
+
+  constructor(public _ms: MapasService) { }
+
+  clickMapa(evento) {
+    const nuevoMarcador: Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: 'Sin titulo',
+      draggable: true
+    };
+
+    this._ms.insertarMarcador(nuevoMarcador);
+
+    console.log(evento);
+  }
+
+
+
 
 }
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from '../interfaces/marcador.interface';
+
 
 @Injectable()
 export class MapasService {
 
-  constructor() { }
+  marcadores: Marcador[] = [];
+
+  constructor() {
+    const nuevoMarcador: Marcador = {
+      lat: 6.246062,
+      lng: -75.589227,
+      titulo: 'Frisby',
+      draggable: true
+    }
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  insertarMarcador(marcador: Marcador) {
+    this.marcadores.push(marcador);
+  }
 
 }
